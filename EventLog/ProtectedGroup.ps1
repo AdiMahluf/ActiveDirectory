9087cd017b23a01c253c37f1e6d106da50035f17
Adi Mahluf <54816430+AdiMahluf@users.noreply.github.com>
2022-05-19 17:23:29 +0300

    Update ProtectedGroup.ps1

--- EventLog/ProtectedGroup.ps1
+++ EventLog/ProtectedGroup.ps1
@@ -3,7 +3,7 @@ $dcs = (Get-ADDomain).ReplicaDirectoryServers
 write-host "[-] Checking if event log is enabled on Domain Controllers" -ForegroundColor Cyan
 foreach ($DC in $dcs) {
 Get-WinEvent -ComputerName $dc -listlog Microsoft-Windows-Authentication/ProtectedUserFailures-DomainController |select @{n="DCNAME";e={$dc}},IsLogFull,IsEnabled
-
+}
 
 #check failed authentication duo to membership in Protected Group
 $dcs = (Get-ADDomain).ReplicaDirectoryServers
